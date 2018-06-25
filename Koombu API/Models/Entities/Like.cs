f50ba70e92d6dee84423fc0cd207ca8573654b75
Romain Foucher <zarnes@live.com>
2018-06-25 17:02:28 +0200

    Populated entities

--- Koombu API/Models/Entities/Like.cs
+++ Koombu API/Models/Entities/Like.cs
@@ -7,5 +7,34 @@ namespace Koombu_API.Models
 {
     public class Like
     {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private int _postId;
+        public int PostId
+        {
+            get { return _postId; }
+            set { _postId = value; }
+        }
+
+        private int _userId;
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public Like() {}
+
+        public Like(int id, int postId, int userId)
+        {
+            Id = id;
+            PostId = postId;
+            UserId = userId;
+        }
     }
 }
