88123783e2ba8c0acf733356867f772357908a31
senraz <meriemkraiem7@gmail.com>
2018-06-25 17:34:50 +0200

    dao

---
+++ Koombu API/Models/DAO/Dao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Koombu_API.Models.DAO
+{
+    public class Dao
+    {
+
+        public Dao()
+        {
+            if (Instance != null)
+                return;
+
+            Instance = this;
+
+            string connectionInitiator = "Database=" + "supfile" + "; ";
+            connectionInitiator += "Data Source=" + "localhost" + "; ";
+
+            connectionInitiator += "User Id=" + "supfileUser" + "; ";
+            connectionInitiator += "Password=" + "ogdTdolLxsFDrrNwI3JB" + "; ";
+
+            connectionInitiator += "SslMode=" + "none" + "; ";
+            Connection = new MySqlConnection(connectionInitiator);
+            Connection.Open();
+        }
+    }
+}
--- Koombu API/Models/DAO/UserDAO.cs
+++ Koombu API/Models/DAO/UserDAO.cs
@@ -14,11 +14,34 @@ namespace Koombu_API.Models.DAO
 
         public User GetUser(int id)
         {
-            return new User()
+            string sql = "SELECT `id`, `firstname`, `lastname`, `birthdate`,`mail`, `company`, `title` FROM `user` WHERE `id` = " + id;
+            MySqlCommand cmd = Database.Instance.Connection.CreateCommand();
+            cmd.CommandText = sql;
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            bool result = reader.Read();
+            if (!result)
             {
-                LastName = "Doe",
-                FirstName = "John"
-            };
+                reader.Close();
+                cmd.Dispose();
+                return null;
+            }
+
+            return new User(
+                reader.GetInt32("id"),
+                reader.GetString("firstname"),
+                reader.GetString("lastname"),
+                reader.GetDate("birthdate"),
+                reader.GetString("mail"),
+                reader.GetString("company"),
+                reader.GetString("title")
+                );
         }
+
+        public void createUser(User user)
+        {
+
+        }
+
     }
 }
